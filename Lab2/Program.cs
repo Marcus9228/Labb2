9f105532d0f077febdd397ba77b8a6a5e7d526d0
marcu <marser@student.chalmers.se>
2023-09-15 21:06:22 +0200

    Added comment

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -9,7 +9,7 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            //För att få appen att fungera se till att ändra pathen specifikt för er dator till txt filerna som ligger i projekt foldern.
+            //För att få appen att fungera se till att ändra pathen både i Customer.cs och Store.cs specifikt för er dator till txt filerna som ligger i projekt foldern.
 
             // Första gången ni startar programmet stäng av och öppna igen för att få tillgång till alla Knatte, Fnatte, Tjatte konton. Och varje gång ni registrerar nytt konto starta om appen för att få discount aktiverad.
 
